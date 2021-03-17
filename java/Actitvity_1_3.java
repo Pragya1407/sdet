7bfbbf46afeea9edeecf1ed5651bd3501ab7c0cc
Pragya1407 <77953939+Pragya1407@users.noreply.github.com>
2021-03-17 11:35:54 +0530

    Add files via upload

---
+++ java/Actitvity_1_3.java
@@ -0,0 +1,33 @@
+package JavaActivity_1_3;
+
+public class Actitvity_1_3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double seconds = 1000000000;
+		
+		double EarthSeconds = 31557600;
+		double MercurySeconds = 0.2408467*31555696;
+		double VenusSeconds = 0.61519726*31555696;
+		double MarsSeconds = 1.8808158*31555696;
+		double JupiterSeconds = 11.862615*31555696;
+		double SaturnSeconds = 29.447498*31555696;
+		double UranusSeconds = 84.016846*31555696;
+		double NeptuneSeconds = 164.79132*31555696;
+		
+		//31.69 Earth-years old = 1,000,000,000 seconds
+		// 1 Earth-years old = 1,000,000,000 / 31.69 = 3,15,55,695.803 seconds
+		
+       System.out.println("MercurySeconds: " +MercurySeconds);
+       System.out.println("VenusSeconds: " +VenusSeconds);
+       System.out.println("MarsSeconds: " +MarsSeconds);
+       System.out.println("JupiterSeconds: " +JupiterSeconds);
+       System.out.println("MarsSeconds: " +MarsSeconds);
+       System.out.println("SaturnSeconds: " +SaturnSeconds);
+       System.out.println("UranusSeconds: " +UranusSeconds);
+       System.out.println("NeptuneSeconds: " +NeptuneSeconds);
+       
+	}
+
+}
