5a406b98c7a1284aba550a810bc26029499e73d3
Pragya1407 <77953939+Pragya1407@users.noreply.github.com>
2021-03-19 00:44:50 +0530

    Add files via upload

---
+++ sql/Activity_1.sql
@@ -0,0 +1,12 @@
+REM   Script: Activity_1
+REM   Create Table
+
+CREATE TABLE SALESMAN( 
+salesman_id int, 
+salesman_name varchar2(20), 
+salesman_city varchar2(20), 
+commission int 
+);
+
+DESCRIBE  SALESMAN;
+
---
+++ sql/Activity_2.sql
@@ -0,0 +1,12 @@
+--Activity_2
+INSERT ALL 
+    INTO SALESMAN Values (5001, 'James Hoog', 'New York', 15) 
+    INTO SALESMAN Values (5002, 'Nail Knite', 'Paris', 13) 
+    INTO SALESMAN Values (5005, 'Pit Alex', 'London', 11) 
+    INTO SALESMAN Values (5006, 'McLyon', 'Paris', 14) 
+    INTO SALESMAN Values (5007, 'Paul Adam', 'Rome', 13)     
+    INTO SALESMAN Values (5003, 'Lauson Hen', 'San Jose', 12) 
+SELECT 1 FROM DUAL;
+
+SELECT * FROM SALESMAN;
+
